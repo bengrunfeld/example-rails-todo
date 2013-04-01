3e8e8ff29c3fe16e0ad5f25c7c70b8a398165b3d
Gabriel Monroy <gabriel@opdemand.com>
2013-04-01 15:58:46 -0600

    initial commit of todos app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleRailsTodo::Application.config.secret_token = '0681f423be8644ca5940ff07b2ba9a60ee176c9e48adf38fee38a05317bb49ea0ba6797e6cb0695ea5a7b838391d10817334a720442e9e211262b73e701328e6'
